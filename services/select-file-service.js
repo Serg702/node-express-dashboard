a526341bfe2a6f99d3c9368612ff71c3cc0f5ff4
Serega K <serega.vegas@gmail.com>
2020-07-07 20:03:47 +0300

    rename read method - SK

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,11 +1,48 @@
+const fs = require("fs");
+const path = require("path");
+const dir = process.cwd();
+
 function getDirectoryContents(files, currentDir, query) {
+  const data = [];
+
+  files.forEach((file) => {
+    const newObj = {
+      name: file,
+      isDirectory: false,
+      path: path.join(query, file),
+      currentDir,
+    };
+    if (isDirectory(currentDir, file)) {
+      newObj.isDirectory = true;
+      data.push(newObj);
+    } else {
+      data.push(newObj);
+    }
+    return data;
+  });
 }
 
 function isDirectory(currentDir, file) {
+  const fileInfo = fs.statSync(path.join(currentDir, file));
+  return fileInfo.isDirectory();
 }
 
 function readDir(currentDir, res, query) {
+  fs.readdir(currentDir, (err, files) => {
+    const directoryContents = [];
+
+    if (!err) {
+      directoryContents = getDirectoryContents(files, currentDir, query);
+    }
+
+    res.json(directoryContents);
+  });
 }
 
 exports.get = (req, res) => {
+  let query = req.query.path;
+
+  if (!query) query = "";
+
+  readDir(dir, res, query);
 };
