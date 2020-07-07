41f979d0ae6784a46d642723f2c7b72f9022559f
Serega K <serega.vegas@gmail.com>
2020-07-07 09:59:34 +0300

    refactor html element getter

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,10 +1,10 @@
 const connection = new WebSocket("ws://localhost:3000");
 
 const logWindow = document.querySelector("#log-window");
-const filePath = document.getElementById("logFilePath");
+const filePath = document.getElementById("logFilePath").value;
 
 connection.onopen = () => {
-  if (filePath) connection.send(filePath.value);
+  if (filePath) connection.send(filePath);
 };
 
 connection.onmessage = (event) => {
