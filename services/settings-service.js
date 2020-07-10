2cfc6042f1caaa36c258313108a260fcc5bd3149
Serega K <serega.vegas@gmail.com>
2020-07-10 07:22:17 +0300

    add getSettings and writeSettings - SK

--- services/settings-service.js
+++ services/settings-service.js
@@ -1,19 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+const settingsFilePath = path.join(__dirname, "json/settings.json");
+
 function getSettings() {
-  return {}
+  const settingsData = fs.readFileSync(settingsFilePath);
+
+  return JSON.parse(settingsData);
 }
 
 function writeSettings(newSettings) {
+  const settingsJSON = JSON.stringify(newSettings, null, 2);
+
+  try {
+    fs.writeFileSync(settingsFilePath, settingsJSON);
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
 
-function getDefaultDir() {
-}
+function getDefaultDir() {}
 
 function isValidDir(dirPath) {
+  try {
+    if (fs.readdirSync(dirPath)) return true;
+  } catch (e) {
+    return false;
+  }
 }
 
 module.exports = {
   getSettings,
   writeSettings,
   getDefaultDir,
-  isValidDir
+  isValidDir,
 };
